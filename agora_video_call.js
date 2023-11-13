0077ea3cd60d7e2905818d540aff81d923c67f5a
truongnt <truongnt@nal.vn>
2023-11-13 16:27:36 +0700

    fsdf

--- agora_video_call.js
+++ agora_video_call.js
@@ -77,24 +77,28 @@ const pages = [];
   setInterval(async () => {
     const page = getRandomElement(pages)
     page.evaluate(() => {
-      function generateRandomString2(length) {
-        const characters = 'abcdefghijklmnopqrstuvwxyz';
-        let result = '';
-      
-        for (let i = 0; i < length; i++) {
-          const randomIndex = Math.floor(Math.random() * characters.length);
-          result += characters.charAt(randomIndex);
+      try {
+        function generateRandomString2(length) {
+          const characters = 'abcdefghijklmnopqrstuvwxyz';
+          let result = '';
+        
+          for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            result += characters.charAt(randomIndex);
+          }
+        
+          return result;
         }
-      
-        return result;
+        window.commentEditor.setData(generateRandomString2(50))
+        $('.btn-submit-chat.text-right.mt-33p.pr-0p').click()
+        const min = 1
+        const max = 6
+        const randomReactionIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+        $(`.comments-screen-zone button:nth-of-type(${randomReactionIndex})`).click()
+        return true
+      } catch (error) {
+        console.log(error);
       }
-      window.commentEditor.setData(generateRandomString2(50))
-      $('.btn-submit-chat.text-right.mt-33p.pr-0p').click()
-      const min = 1
-      const max = 6
-      const randomReactionIndex = Math.floor(Math.random() * (max - min + 1)) + min;
-      $(`.comments-screen-zone button:nth-of-type(${randomReactionIndex})`).click()
-      return true
     })
   }, 500)
 })();
