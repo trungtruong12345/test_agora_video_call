6eae6310320119bcab7a0e1a7df1c1807dbf2c62
truongnt <truongnt@nal.vn>
2023-11-24 17:42:21 +0700

    dfds

--- agora_video_call.js
+++ agora_video_call.js
@@ -22,7 +22,7 @@ const CHECKIN_LINK = "https://msm.majisemi.com/check_in/1996"
 const users = [];
 const strRandom = generateRandomString(10)
 
-for (let i = 1; i <= 25; i++) {
+for (let i = 1; i <= 13; i++) {
   users.push({
     email: `test${i}_${strRandom}@gmail.com`,
     username: `test${i}_${strRandom}`,
