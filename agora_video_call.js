6d7e16278278c39abaa00d846cc3b2d30f708975
truongnt <truongnt@nal.vn>
2023-11-27 17:30:24 +0700

    dfa

--- agora_video_call.js
+++ agora_video_call.js
@@ -1,104 +1,138 @@
 const puppeteer = require("puppeteer")
 
 function getRandomElement(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
+  const randomIndex = Math.floor(Math.random() * array.length)
+  return array[randomIndex]
+}
+
+function consoFunc(mess) {  
+  console.log(mess);
 }
 
 function generateRandomString(length) {
-  const characters = 'abcdefghijklmnopqrstuvwxyz';
-  let result = '';
+  const characters = "abcdefghijklmnopqrstuvwxyz"
+  let result = ""
 
   for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    result += characters.charAt(randomIndex);
+    const randomIndex = Math.floor(Math.random() * characters.length)
+    result += characters.charAt(randomIndex)
   }
 
-  return result;
+  return result
 }
 
 const CHECKIN_LINK = "https://msm.majisemi.com/check_in/1998"
 
-const users = [];
+const users = []
 const strRandom = generateRandomString(10)
 
 for (let i = 1; i <= 25; i++) {
   users.push({
     email: `test${i}_${strRandom}@gmail.com`,
     username: `test${i}_${strRandom}`,
-  });
+  })
 }
-const pages = [];
+const pages = []
 
-(async () => {
-  const browser = await puppeteer.launch({headless: 'new'});
+;(async () => {
+  const browser = await puppeteer.launch({ headless: "new" })
 
   for (let i = 0; i < users.length; i++) {
-    const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
+    const page = await browser.newPage()
+    await page.setDefaultNavigationTimeout(0)
 
-    const client = users[i];
-    const email = client.email;
-    const nickname = client.username;
-    const iconIndex = Math.floor(Math.random() * 15) + 1;
+    const client = users[i]
+    const email = client.email
+    const nickname = client.username
+    const iconIndex = Math.floor(Math.random() * 15) + 1
 
-    const status = await page.goto(CHECKIN_LINK);
+    const status = await page.goto(CHECKIN_LINK)
 
-    await page.waitForSelector('input[type="text"][name="email"][id="email"]');
-    await page.waitForSelector('input[type="text"][name="nickname"][id="nickname"]');
-    await page.waitForSelector(`.grid-template-columns-auto-10.d-grid.grid-row-gap-10`);
-    await page.waitForSelector(`.w-300p.p-12p.font-size-21[type="submit"]`);
-    const iconElement = await page.waitForSelector(`.grid-template-columns-auto-10.d-grid.grid-row-gap-10 > div:nth-of-type(${iconIndex})`);
+    await page.waitForSelector('input[type="text"][name="email"][id="email"]')
+    await page.waitForSelector(
+      'input[type="text"][name="nickname"][id="nickname"]'
+    )
+    await page.waitForSelector(
+      `.grid-template-columns-auto-10.d-grid.grid-row-gap-10`
+    )
+    await page.waitForSelector(`.w-300p.p-12p.font-size-21[type="submit"]`)
+    const iconElement = await page.waitForSelector(
+      `.grid-template-columns-auto-10.d-grid.grid-row-gap-10 > div:nth-of-type(${iconIndex})`
+    )
 
-    await page.type('input[type="text"][name="email"][id="email"]', email);
-    await page.type('input[type="text"][name="nickname"][id="nickname"]', nickname);
-    await iconElement.click();
+    await page.type('input[type="text"][name="email"][id="email"]', email)
+    await page.type(
+      'input[type="text"][name="nickname"][id="nickname"]',
+      nickname
+    )
+    await iconElement.click()
 
-    const isChecked = await page.$eval('#agree_terms', (element) => element.checked);
+    const isChecked = await page.$eval(
+      "#agree_terms",
+      (element) => element.checked
+    )
     if (!isChecked) {
-      await page.click(`.p-application_form__content.p-0.m-0.border-unset.bg-white.d-flex.flex-column`);
+      await page.click(
+        `.p-application_form__content.p-0.m-0.border-unset.bg-white.d-flex.flex-column`
+      )
     }
 
-    const currentURL = await page.url();
-    console.log('currentURL', currentURL)
+    const currentURL = await page.url()
+    console.log("currentURL", currentURL)
     const [response] = await Promise.all([
       page.click(`.w-300p.p-12p.font-size-21[type="submit"]`),
-      page.waitForNavigation({ waitUntil: 'load' }),
-    ]);
+      page.waitForNavigation({ waitUntil: "load" }),
+    ])
 
-    const seminarUrl = await page.url();
-    console.log('seminar url', seminarUrl) 
-    await page.setDefaultNavigationTimeout(0);
+    const seminarUrl = await page.url()
+    console.log("seminar url", seminarUrl)
+    await page.setDefaultNavigationTimeout(0)
+    console.log(i)
+
     // await page.waitForSelector('.comments-screen-zone')
     // await page.waitForSelector('.ck.ck-reset.ck-editor.ck-rounded-corners')
     // await page.waitForSelector('.btn-submit-chat.text-right.mt-33p.pr-0p')
     pages.push(page)
+    page.evaluate(() => {
+      console.log('init onbeforeunload');
+      try {
+        window.onbeforeunload = function (event) {
+          console.log('Reload page !!!');
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    })
   }
+
   setInterval(async () => {
     const page = getRandomElement(pages)
     page.evaluate(() => {
       try {
         function generateRandomString2(length) {
-          const characters = 'abcdefghijklmnopqrstuvwxyz';
-          let result = '';
-        
+          const characters = "abcdefghijklmnopqrstuvwxyz"
+          let result = ""
+
           for (let i = 0; i < length; i++) {
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            result += characters.charAt(randomIndex);
+            const randomIndex = Math.floor(Math.random() * characters.length)
+            result += characters.charAt(randomIndex)
           }
-        
-          return result;
+
+          return result
         }
         window.commentEditor.setData(generateRandomString2(50))
-        $('.btn-submit-chat.text-right.mt-33p.pr-0p').click()
+        $(".btn-submit-chat.text-right.mt-33p.pr-0p").click()
         const min = 1
         const max = 6
-        const randomReactionIndex = Math.floor(Math.random() * (max - min + 1)) + min;
-        $(`.comments-screen-zone button:nth-of-type(${randomReactionIndex})`).click()
+        const randomReactionIndex =
+          Math.floor(Math.random() * (max - min + 1)) + min
+        $(
+          `.comments-screen-zone button:nth-of-type(${randomReactionIndex})`
+        ).click()
         return true
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
     })
-  }, 300)
-})();
+  }, 200)
+})()
